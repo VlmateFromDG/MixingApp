03ef60db26705e2e226cbcc0e170081f1d3a01e0
Den <Den@DESKTOP-E5HR271>
2017-09-17 11:55:25 +0300

    start sportify

---
+++ MixingApp.Services/SpotifyService.cs
@@ -0,0 +1,53 @@
+﻿using SpotifyAPI.Local;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+using System;
+using Newtonsoft.Json;
+using System.Net;
+using System.Collections.Specialized;
+using System.Text;
+using System.IO;
+
+namespace MixingApp.Services
+{
+    public class SpotifyService
+    {
+        private string ClientId = "385f6403c4424a7aa4700704f3dd5284";
+        private string ClientSecret = "9cdac36957504632b2651e7a4c477f2e";
+        private string OauthKey;
+        public Scope Scope { get; set; }
+        public object JsonConvert { get; private set; }
+
+        public void GetLinksSongs()
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers.Add("Authorization",
+                    "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(ClientId + ":" + ClientSecret)));
+
+                NameValueCollection col = new NameValueCollection
+                {
+                    {"grant_type", "client_credentials"},
+                    {"scope", Scope.GetStringAttribute(" ")}
+                };
+
+                byte[] data;
+                try
+                {
+                    data = wc.UploadValues("https://accounts.spotify.com/api/token", "POST", col);
+                }
+                catch (WebException ex)
+                {
+                    using (StreamReader reader = new StreamReader(ex.Response.GetResponseStream()))
+                    {
+                        data = Encoding.UTF8.GetBytes(reader.ReadToEnd());
+                    }
+                }
+                var token = Newtonsoft.Json.JsonConvert.DeserializeObject<Token>(Encoding.UTF8.GetString(data));
+
+                OauthKey = token.AccessToken;
+            }
+        }
+    }
+}
--- MixingApp/MainWindow.xaml.cs
+++ MixingApp/MainWindow.xaml.cs
@@ -38,6 +38,7 @@ namespace MixingApp
     public partial class MainWindow : Window
     {
         private BassNetService _bassNetService;
+        private SpotifyService _spotifyService;
         private string ClientId = "385f6403c4424a7aa4700704f3dd5284";
         private string ClientSecret = "9cdac36957504632b2651e7a4c477f2e";
         private string OauthKey;
@@ -48,6 +49,7 @@ namespace MixingApp
         public MainWindow()
         {
             _bassNetService = new BassNetService();
+            _spotifyService = new SpotifyService();
             BassNetRegistration.CallBassNetRegistration();
             //RunSpotify();
             _config = new SpotifyLocalAPIConfig();
@@ -62,35 +64,7 @@ namespace MixingApp
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //using (WebClient wc = new WebClient())
-            //{
-            //    wc.Headers.Add("Authorization",
-            //        "Basic " + Convert.ToBase64String(Encoding.UTF8.GetBytes(ClientId + ":" + ClientSecret)));
-
-            //    NameValueCollection col = new NameValueCollection
-            //    {
-            //        {"grant_type", "client_credentials"},
-            //        {"scope", Scope.GetStringAttribute(" ")}
-            //    };
-
-            //    byte[] data;
-            //    try
-            //    {
-            //        data = wc.UploadValues("https://accounts.spotify.com/api/token", "POST", col);
-            //    }
-            //    catch (WebException ex)
-            //    {
-            //        using (StreamReader reader = new StreamReader(ex.Response.GetResponseStream()))
-            //        {
-            //            data = Encoding.UTF8.GetBytes(reader.ReadToEnd());
-            //        }
-            //    }
-            //    var token = JsonConvert.DeserializeObject<Token>(Encoding.UTF8.GetString(data));
-
-            //    OauthKey = token.AccessToken;
-            //}
-            //SendPlayRequest("https://open.spotify.com/track/4myBMnNWZlgvVelYeTu55w", "");
-
+            _spotifyService.GetLinksSongs();
             //var urlTrack = new List<string>();
             //urlTrack.Add("https://open.spotify.com/track/2TpxZ7JUBn3uw46aR7qd6V");
             //urlTrack.Add("https://open.spotify.com/track/4PjcfyZZVE10TFd9EKA72r");
@@ -100,129 +74,9 @@ namespace MixingApp
             //urlTrack.Add("https://p.scdn.co/mp3-preview/4a54d83c195d0bc17b1b23fc931d37fb363224d8?cid=8897482848704f2a8f8d7c79726a70d4");
             //urlTrack.Add("https://p.scdn.co/mp3-preview/fce49876156ffb50ecc873e0fc7e1714bc03f10b?cid=8897482848704f2a8f8d7c79726a70d4");
 
+            //_bassNetService.CallBassNetRegistration();
 
-            //BassNet.StartBass();
-
-            _bassNetService.CallBassNetRegistration();
-
-            _bassNetService.Play();
+            //_bassNetService.Play();
         }
-
-
-
-
-        //internal async Task SendPlayRequest(string url, string context = "")
-        //{
-        //    // TODO: instead of having an empty context, one way to fix the bug with the playback time beyond the length of a song would be to provide a 1-song context, and it would be fixed.
-        //    await QueryAsync($"remote/play.json?uri={url}&context={context}", true, true, -1).ConfigureAwait(false);
-        //}
-
-        //internal async Task<string> QueryAsync(string request, bool oauth, bool cfid, int wait)
-        //{
-        //    string parameters = "?&ref=&cors=&_=" + GetTimestamp();
-        //    if (request.Contains("?"))
-        //    {
-        //        parameters = parameters.Substring(1);
-        //    }
-
-        //    if (oauth)
-        //    {
-        //        parameters += "&oauth=" + OauthKey;
-        //    }
-        //    if (cfid)
-        //    {
-        //        parameters += "&csrf=" + CfidKey;
-        //    }
-
-        //    if (wait != -1)
-        //    {
-        //        parameters += "&returnafter=" + wait;
-        //        parameters += "&returnon=login%2Clogout%2Cplay%2Cpause%2Cerror%2Cap";
-        //    }
-
-        //    string address = $"{_config.HostUrl}:{_config.Port}/{request}{parameters}";
-        //    string response = "";
-        //    try
-        //    {
-        //        using (var wc = new ExtendedWebClient())
-        //        {
-        //            if (SpotifyLocalAPI.IsSpotifyRunning())
-        //                response = "[ " + await wc.DownloadStringTaskAsync(new Uri(address)).ConfigureAwait(false) + " ]";
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        return string.Empty;
-        //    }
-
-        //    return response;
-        //}
-        //public static void RunSpotify()
-        //{
-        //    if (!IsSpotifyRunning() && File.Exists(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"spotify\spotify.exe")))
-        //        Process.Start(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"spotify\spotify.exe"));
-        //}
-        //public static Boolean IsSpotifyRunning()
-        //{
-        //    return Process.GetProcessesByName("spotify").Length >= 1;
-        //}
-        //internal int GetTimestamp()
-        //{
-        //    return Convert.ToInt32((DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds);
-        //}
-
-        //internal string GetCfid()
-        //{
-        //    string response = Query("simplecsrf/token.json", false, false, -1);
-        //    response = response.Replace(@"\", "");
-        //    List<Cfid> cfidList = (List<Cfid>)JsonConvert.DeserializeObject(response, typeof(List<Cfid>));
-        //    if (cfidList == null)
-        //        return "";
-        //    if (cfidList.Count != 1)
-        //        throw new Exception("CFID could not be loaded");
-        //    return cfidList[0].Error == null ? cfidList[0].Token : "";
-        //}
-        //internal string Query(string request, bool oauth, bool cfid, int wait)
-        //{
-        //    string parameters = "?&ref=&cors=&_=" + GetTimestamp();
-        //    if (request.Contains("?"))
-        //    {
-        //        parameters = parameters.Substring(1);
-        //    }
-
-        //    if (oauth)
-        //    {
-        //        parameters += "&oauth=" + OauthKey;
-        //    }
-        //    if (cfid)
-        //    {
-        //        parameters += "&csrf=" + CfidKey;
-        //    }
-
-        //    if (wait != -1)
-        //    {
-        //        parameters += "&returnafter=" + wait;
-        //        parameters += "&returnon=login%2Clogout%2Cplay%2Cpause%2Cerror%2Cap";
-        //    }
-
-        //    string address = $"{_config.HostUrl}:{_config.Port}/{request}{parameters}";
-        //    string response = "";
-        //    try
-        //    {
-        //        using (var wc = new ExtendedWebClient())
-        //        {
-        //            if (SpotifyLocalAPI.IsSpotifyRunning())
-        //            {
-        //                response = "[ " + wc.DownloadString(address) + " ]";
-        //            }
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        return string.Empty;
-        //    }
-
-        //    return response;
-        //}
     }
 }
